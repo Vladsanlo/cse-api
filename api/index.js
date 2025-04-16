b4bccd57847f44b9e10f30f17207ea60b21f00d6
Vladimir Sanchez <vladypuggu@gmail.com>
2025-04-16 16:54:14 -0600

    Created index.js page

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+// const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// require("express-async-errors");
+
+app.use(express.json());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.com']
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error);
+    }
+  }
+}
+
+// app.use(cors());
+
+//routerApi(app)
+
+app.get('/api', (req, res) => {
+  res.send('Api de la serie Common Side Effects');
+})
+
+app.get('/api/nueva-ruta', (req, res) => {
+  res.send('Nueva Ruta');
+})
+
+//routerApi(app);
+
+app.listen(port, () => {
+  console.log('mi port' + port)
+})
