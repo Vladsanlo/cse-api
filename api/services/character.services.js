9f0daf1a693c2d68135758bce522fdc19e9b8407
Vladimir Sanchez <vladypuggu@gmail.com>
2025-04-22 01:19:13 -0600

    Added const router=express.Router()
    Created  characters array
    
    Created router for characters
    
    Created const foundCharacter
    
    Created if/else for constant foundCharacter
    
    Created post for newCharacter
    
    Added export for router

--- api/services/character.services.js
+++ api/services/character.services.js
@@ -1,38 +1,49 @@
 const express = require('express');
+const router = express.Router();
+
+let characters = [
+  {
+    id: 1,
+    name: 'jose',
+    type: 'employee',
+    image: ''
+  },
+  {
+    id: 2,
+    name: 'katy',
+    type: 'customer'
+  },
+  {
+    id: 3,
+    name: "Hildy",
+    type: "Culera"
+  }
+]
 
 router.get('/', (req, res) => {
-  res.json([
-    {
-      name: 'jose',
-      type: 'employee'
-    },
-    {
-      name: 'katy',
-      type: 'customer'
-    }
-  ])
+  res.json(characters)
 })
 //ejercicio
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  res.json({
-    id: id,
-    name: 'marshal cuso',
-    type: 'employee',
-  })
+  const foundCharacter = characters.find(({id: character_id}) => character_id == id)
+
+  if (foundCharacter)
+    res.json(foundCharacter)
+  else
+    res.json({error: 'Not found'})
 })
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
+router.post('/', (req, res) => {
+  const { name, type } = req.body;
+  const newCharacter =  { id: characters.length + 1,  name: name, type: type }
 
-  res.json({
-    id: id,
-    name: 'frances applewhite',
-    type: 'employee',
-  })
+  characters = [...characters, newCharacter]
+
+  res.json(newCharacter)
 })
 
 
 
-module.exports = CharactersService
+module.exports = router;
