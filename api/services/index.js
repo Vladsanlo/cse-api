7925952dbb26c3024aa095143fec0c7b466c748c
Vladimir Sanchez <vladypuggu@gmail.com>
2025-04-22 01:20:20 -0600

    Created const routerApi
    Created router for characterservice
    
    Added export for routerApi

--- api/services/index.js
+++ api/services/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const charactersservice=require('./character.services')
 
-
-const router = express.Router();
+const routerApi = app => {
+  //path global   {
+  const router = express.Router();
   app.use('/api/v1',router);
+  //  } sirve para "generalizar" una ruta,en este caso,todos van a usar /api/v1
+  router.use('/service', charactersservice);
+}
+module.exports=routerApi;
