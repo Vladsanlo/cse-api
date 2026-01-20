4b728ede298c3085ae45023460028107e3971496
Vladimir Sanchez <vladypuggu@gmail.com>
2026-01-19 23:52:34 -0600

    Updated character service reference

--- api/services/index.js
+++ api/services/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const charactersservice=require('./character.services')
+const charactersService = require('./character.service')
 
 const routerApi = app => {
   //path global   {
   const router = express.Router();
-  app.use('/api/v1',router);
+  app.use('/api/v1', router);
   //  } sirve para "generalizar" una ruta,en este caso,todos van a usar /api/v1
-  router.use('/service', charactersservice);
+  router.use('/characters', charactersService);
 }
-module.exports=routerApi;
+
+module.exports = routerApi;
